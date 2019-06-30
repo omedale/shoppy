2467a13963c76e8a754ef111816aef768b354eaa
omedale <omedale@gmail.com>
2019-06-30 11:41:18 +0100

    refactor search products

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -12,12 +12,19 @@ use App\Helpers\ErrorHelper;
 
 class ProductController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request, Product $product) {
         $data = $this->buildData($request);
         $description_length = $data['description_length'];
+
+        $products = Product::query();
+        if (isset($request->filter)) {
+            $filter_data = json_decode($request->filter);
+            $products = $this->filterProducts($filter_data, $products, $product);
+        }
+
         return response()->json([
-            'count' => Product::count(),
-            'rows' => Product::with('attributes')
+            'count' => $products->count(),
+            'rows' => $products
                     ->select(DB::raw("product_id, name, substr(description, 1, $description_length) as description, price, discounted_price, thumbnail, image, image_2"))
                     ->offset($data['offset'])->limit($data['limit'])->get()
         ]);
@@ -44,14 +51,7 @@ class ProductController extends Controller
         $products = Product::query();
         if (isset($request->filter)) {
             $filter_data = json_decode($request->filter);
-            $has_attribute_filter = isset($filter_data->attribute_value_ids) && count($filter_data->attribute_value_ids) > 0;
-            $has_category_filter = isset($filter_data->category_ids) && count($filter_data->category_ids) > 0;
-            $has_department_filter = isset($filter_data->department_ids) && count($filter_data->department_ids) > 0;
-            $has_price_range = isset($filter_data->price_range) && count($filter_data->price_range) > 0;
-            $products = $has_department_filter ? $product->filterByDepartments($products, $filter_data->department_ids) : $products;
-            $products = $has_attribute_filter ? $product->filterByAttributes($products, $filter_data->attribute_value_ids) : $products;
-            $products = $has_category_filter ? $product->filterByCatgories($products, $filter_data->category_ids) : $products;
-            $products = $has_price_range ? $product->orWhereBetween('price', $filter_data->price_range) : $products;
+            $products = $this->filterProducts($filter_data, $products, $product);
         }
 
         $products = $product->searchProduct($products, $data, $this->fullTextSearchTerm($term, $data['all_words']));
@@ -59,6 +59,26 @@ class ProductController extends Controller
             'count' => $products->count(),
             'rows' => $products->offset($data['offset'])->limit($data['limit'])->get()
         ]);
+    }
+
+    private function filterProducts($filter_data, $products, $product) {
+        $has_attribute_filter = isset($filter_data->attribute_value_ids) && count($filter_data->attribute_value_ids) > 0;
+        $has_category_filter = isset($filter_data->category_ids) && count($filter_data->category_ids) > 0;
+        $has_department_filter = isset($filter_data->department_ids) && count($filter_data->department_ids) > 0;
+        $has_price_range = isset($filter_data->price_range) && count($filter_data->price_range) > 0;
+
+        $has_max_min_range = $has_price_range && count($filter_data->price_range) == 2 && $filter_data->price_range[0] > 0 && $filter_data->price_range[1] > 0 && $filter_data->price_range[0] <= $filter_data->price_range[1];
+        $has_only_min = !$has_max_min_range && $has_price_range && count($filter_data->price_range) == 2 && $filter_data->price_range[0] > 0 && $filter_data->price_range[1] <= 0;
+        $has_only_max = !$has_max_min_range && $has_price_range && count($filter_data->price_range) == 2 && $filter_data->price_range[0] <= 0 && $filter_data->price_range[1] > 0;
+
+        $products = $has_department_filter ? $product->filterByDepartments($products, $filter_data->department_ids) : $products;
+        $products = $has_attribute_filter ? $product->filterByAttributes($products, $filter_data->attribute_value_ids) : $products;
+        $products = $has_category_filter ? $product->filterByCatgories($products, $filter_data->category_ids) : $products;
+
+        $products = $has_max_min_range ? $product->orWhereBetween('price', $filter_data->price_range) : $products;
+        $products = $has_only_min ? $product->orWhere('price', '>=', (float)$filter_data->price_range[0]) : $products;
+        $products = $has_only_max ? $product->orWhere('price', '<=', $filter_data->price_range[1]) : $products;
+        return $products;
     }
 
     private function buildData($request) {
